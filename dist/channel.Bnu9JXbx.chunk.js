a4579530114632998b9d77d63fbe4cf3ffadf1f1
627859663-del <627859663@qq.com>
2026-09-07 17:17:21 +0800

    release: sync regex and audit fixes from 55d8553

---
+++ dist/channel.Bnu9JXbx.chunk.js
@@ -0,0 +1,1 @@
+import{U as r,C as a}from'./mermaid.core.CbhS3dwP.chunk.js';const o=(o,s)=>r.lang.round(a.parse(o)[s]);export{o as c};
---
+++ dist/chunk-2Q5K7J3B.DMpVIQzQ.chunk.js
@@ -0,0 +1,1 @@
+import{_ as t}from'./mermaid.core.CbhS3dwP.chunk.js';var i=class{constructor(t){this.init=t,this.records=this.init()}static{t(this,'ImperativeState')}reset(){this.records=this.init()}};export{i as I};
